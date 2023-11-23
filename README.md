e9d48b9b643ab0ae98bc2ac1d8517acd58b7f713
rootnure <nur.diu.2791@gmail.com>
2023-11-23 13:03:23 +0600

    Delete route added for cart data

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 ## Work Continue From [This Repository](https://github.com/rootnure/bistro-boss-part-3-server-66)
 
 ## Today's Work
-- 
+
+- Get route added for cart
+- Post route added for cart
+- Delete route added for cart
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -44,14 +44,22 @@ async function run() {
         // cart related api
         /* carts collection */
         app.get("/cart", async(req,res) => {
-            const cursor = {email: req.query.email}
-            const result = await cartCollection.find(cursor).toArray();
+            const email= req.query.email;
+            const query = {email: email}
+            const result = await cartCollection.find(query).toArray();
             res.send(result);
         })
 
         app.post("/cart", async(req,res) => {
             const cart = req.body;
             const result = await cartCollection.insertOne(cart);
+            res.send(result);
+        })
+
+        app.delete("/cart/:id", async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result =await cartCollection.deleteOne(query);
             res.send(result);
         })
 
